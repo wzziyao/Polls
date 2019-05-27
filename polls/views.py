677f0b3b4a0eee43832918034b371beffb740d1d
Ziyao Wang <46012949+zziyaow@users.noreply.github.com>
2019-05-26 23:18:40 -0400

    Update views.py

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,43 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.utils import timezone
+from django.template import loader
+from django.urls import reverse
+
+from .models import Question, Choice
+
 def index(request):
-    return HttpResponse("2019 Summer Incubator!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    try:
+        selected_choice = question.choice_set.get(id=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # always return an HttpResponseRedirect after successfully dealing
+        # with a POST data. This prevents data from being posted twice if a
+        # user hits the Back button
+        return HttpResponseRedirect(reverse('polls:results', args=[question.id]))
